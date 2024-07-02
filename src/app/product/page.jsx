ded6f6b0edca25eeaf5536965864a597f410c386
Your Name <you@example.com>
2024-07-02 11:44:51 +0530

    Routing

---
+++ src/app/product/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const product = () => {
+  return (
+    <>
+    <h1>product page</h1>
+    <hr />
+    <p>Lorem ipsum dolor sit amet consectetur
+         adipisicing elit. Voluptate provident, et sit eaque
+          similique minima nam non aperiam placeat 
+        repellendus iste beatae inventore, modi
+         ipsa accusamus amet quam dolor officia.</p>
+    </>
+  )
+}
+
+export default product
